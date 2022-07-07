6896c60bf179bdc44df58b2643b7f9bb7d4adf64
Siddartha Mallick <mallicksiddartha@gmail.com>
2022-07-07 07:44:45 +0600

    Added global context.

--- src/App.js
+++ src/App.js
@@ -7,20 +7,23 @@ import Header from './components/Header';
 import Home from './components/Home';
 import Movie from './components/Movie';
 import NotFound from './components/NotFound';
+//contexts
+import UserProvider from './context';
 
 //styles
 import { GlobalStyle } from './GlobalStyles';
 
 const App = () => (
   <Router>
-    <Header />
-    <Routes>
-      <Route path='/' element={ <Home /> }/>
-      <Route path='/:movieId' element={ <Movie /> } />
-      <Route path='/*' element={ <NotFound /> } />
-    </Routes>
-    
-    <GlobalStyle />
+    <UserProvider>
+      <Header />
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/:movieId' element={<Movie />} />
+        <Route path='/*' element={<NotFound />} />
+      </Routes>
+      <GlobalStyle />
+    </UserProvider>
   </Router>
 );
 
